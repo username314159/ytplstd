71f64f97633120cb753c48dd1984ab24412ee069
username314159 <mirsadcg@gmail.com>
2021-03-01 13:41:32 +0100

    initial commit

---
+++ downloadscript.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 
+import os 
+import subprocess
+
+from pytube import YouTube, Playlist
+
+url =input('Enter the playlist link you want to download:')
+pl = Playlist(url)
+dest_path = input ('Please specify a saving destination:')
+print(f'Downloading: {pl.title} to {dest_path}')
+
+for url in pl:
+    YouTube(url).streams.filter(only_audio=True).first().download(dest_path)
+
+
+from pytube import Playlist
