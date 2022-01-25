cab90ca24009a948ee383706d58bf4c5a6a76bfe
username314159 <mirsadcg@gmail.com>
2022-01-26 00:55:12 +0100

    Added gitignore file

--- downloadscript.py
+++ downloadscript.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python 
 import os 
 import subprocess
 
@@ -7,7 +6,7 @@ from pytube import YouTube, Playlist
 url =input('Enter the playlist link you want to download:')
 pl = Playlist(url)
 dest_path = input ('Please specify a saving destination:')
-print(f'Downloading: {pl.title} to {dest_path}')
+print(f'Downloading: {pl.title} to {dest_path}...')
 
 for url in pl:
     YouTube(url).streams.filter(only_audio=True).first().download(dest_path)
