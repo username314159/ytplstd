e38e60ff9287946830a9ae7a7009bca492e30c38
username314159 <mirsadcg@gmail.com>
2022-06-02 02:14:01 +0200

    ffmpeg conversion to mp3

--- downloadscript.py
+++ downloadscript.py
@@ -1,14 +1,23 @@
 import os 
+import glob
 import subprocess
 
 from pytube import YouTube, Playlist
 
-url =input('Enter the playlist link you want to download:')
+url =input('Enter the playlist link you want to download: ')
 pl = Playlist(url)
-dest_path = input ('Please specify a saving destination:')
-print(f'Downloading: {pl.title} to {dest_path}...')
+dest_path = input ('Please specify a saving destination: ')
+print(f'Downloading playlist: {pl.title} to {os.path.join(os.getcwd(), dest_path)}/')
 
 for url in pl:
-    YouTube(url).streams.filter(only_audio=True).first().download(dest_path)
+    song = YouTube(url).streams.filter(only_audio=True).first().download(dest_path)
+    out_name = song.replace("mp4", "mp3")
+    print(f'Downloading and converting to mp3 song {song}')
+    out = subprocess.Popen(['ffmpeg', '-i', f"{song}", '-acodec','libmp3lame', '-q:a', '2', f"{out_name}"])
 
-
+for i in glob.glob(os.path.join(os.getcwd(),dest_path,"*.mp4")):
+    try:
+       os.chmod(i,0o777)
+       os.remove(i)
+    except OSError:
+       pass
--- pjesme/convert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-for f in *.mp4
-do
-       	ffmpeg -i "$f" -acodec libmp3lame -q:a 2 "${f%.*}.mp3" && rm "$f"
-done
